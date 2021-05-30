c573ea2e4c34aee202984bc9d7b641e883265876
ErinyZarif <48393732+ErinyZarif@users.noreply.github.com>
2021-05-30 21:04:53 +0200

    Update Parser_Table.cpp
    follow

--- src/Parser_Table.cpp
+++ src/Parser_Table.cpp
@@ -1,5 +1,7 @@
 #include "Parser_Table.h"
 #include <iostream>
+#include <algorithm>
+#include <string.h>
 
 Parser_Table::Parser_Table(Grammer* g){
   grammer = g;
@@ -11,8 +13,12 @@ Parser_Table::Parser_Table(Grammer* g){
     get_frist(&(*it));
     }
   }
-
-  get_follow();
+  for(it=this->grammer->non_terminals.begin();it!=this->grammer->non_terminals.end();it++){
+        cout << "Non Terminal: " << it->name << endl;
+    if((*it).follow.empty()){
+    get_follow(&(*it));
+    }
+  }
 
   construct_table();
 }
@@ -41,8 +47,55 @@ Parser_Table::get_frist(Non_terminal* nonterm){
 
 }
 
-Parser_Table::get_follow(){
+Parser_Table::get_follow(Non_terminal* nonterminal){
 
+
+    if (nonterminal->id == 0){
+        nonterminal->follow.push_back(-1);
+    }
+
+    list<rule>::iterator it;
+    for(it = grammer->allRules.begin(); it != grammer->allRules.end(); it++){
+        list<Node>::iterator n;
+        for(n = it->to.begin(); n != it->to.end(); n++){
+            //Node next = *(std::next(n,1));
+            if(n->name.compare(nonterminal->name) == 0 && std::next(n,1) != it->to.end() ){
+                if(std::next(n,1)->terminal){
+                    int id = grammer->get_terminal_id(std::next(n,1)->name);
+                    if(!(std::find(nonterminal->follow.begin(), nonterminal->follow.end(), id) != nonterminal->follow.end()) && id != 0){
+                        nonterminal->follow.push_back(id);
+                    }
+                }
+                else{
+                    Non_terminal* nt = grammer->get_non_terminal(std::next(n,1)->name);
+                    list<int>::iterator y;
+                    for(y = nt->first.begin(); y != nt->first.end(); y++){
+                        if(!(std::find(nonterminal->follow.begin(), nonterminal->follow.end(), (*y)) != nonterminal->follow.end()) && (*y)!=0 ){
+                            nonterminal->follow.push_back(*y);
+                        }
+                    }
+                    if(std::find(nt->first.begin(), nt->first.end(), 0/*id of eps*/) != nt->first.end()){
+                        //int nontermID = grammer->it->non_terminal;
+                        Non_terminal* nonterm = grammer->get_non_terminal(it->non_terminal);
+                        for(y = nonterm->follow.begin(); y != nonterm->follow.end(); y++){
+                            if(!(std::find(nonterminal->follow.begin(), nonterminal->follow.end(), (*y)) != nonterminal->follow.end())&& (*y)!=0 ){
+                                nonterminal->follow.push_back(*y);
+                            }
+                        }
+                    }
+                }
+            }
+            else if(n->name.compare(nonterminal->name) == 0 && std::next(n,1) == it->to.end()){
+                Non_terminal* ntt = grammer->get_non_terminal(it->non_terminal);
+                list<int>::iterator y;
+                for(y = ntt->follow.begin(); y != ntt->follow.end(); y++){
+                    if(!(std::find(nonterminal->follow.begin(), nonterminal->follow.end(), (*y)) != nonterminal->follow.end())&& (*y)!=0 ){
+                        nonterminal->follow.push_back(*y);
+                    }
+                }
+            }
+        }
+    }
 }
 
 Parser_Table::construct_table(){
